a4e29b0140f8c06d1dfef2a8432fa6d1ef1277c5
keiiwami <kei6023.mcpe@gmail.com>
2021-12-26 00:34:56 +0900

    First Commit
    First Commit

---
+++ negoto/app/negoto.swift
@@ -0,0 +1,17 @@
+//
+//  negotoApp.swift
+//  Shared
+//
+//  Created by Kei Iwami on 2021/12/25.
+//
+
+import SwiftUI
+
+@main
+struct negoto: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
